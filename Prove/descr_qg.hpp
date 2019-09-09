2f96384ff902a91bee438a6bc6169be94ae25942
Andrea Di Primio <andrea.diprimio@mail.polimi.it>
2019-09-09 15:34:45 +0200

    Added import routines, added some corrections.

--- Prove/descr_qg.hpp
+++ Prove/descr_qg.hpp
@@ -1,4 +1,4 @@
-// Struct to contain GetFEM++ descriptors. To be completed.
+// Struct to contain GetFEM++ descriptors.
 
 #ifndef FEMG_DESCR_QG_HPP
 #define FEMG_DESCR_QG_HPP
@@ -26,7 +26,17 @@ namespace getfem {
 		//Output directory
 		std::string OUTPUT;
 
-		//TBA: solve descriptors, import routine
+		//Solver descriptor
+		std::string SOLVE_METHOD;
+
+		//Max number of iterations (for iterative solvers)
+		size_type MAX_ITER;
+
+		//Residue tolerance (for iterative solvers)
+		scalar_type TOL;
+
+		//Input file
+		ftool::md_param FILE_;
 
 		friend std::ostream & operator << (
 		std::ostream & out, const descr_qg & descr
@@ -40,10 +50,35 @@ namespace getfem {
 			std::cout << " FEM TYPE (SOLUTION)       : " << descr.FEM_TYPEG   << std::endl;
 			std::cout << " FEM TYPE (COEFFICIENTS)   : " << descr.FEM_TYPEG_DATA << std::endl;
 			std::cout << "--------------------------------------------------" << std::endl;
-
+			std::cout << " SOLVER                    : " << descr.SOLVE_METHOD << std::endl;
+			std::cout << " MAX ITERATIONS            : " << descr.MAX_ITER    << std::endl;
+			std::cout << " TOLERANCE                 : " << descr.TOL         << std::endl;
+			std::cout << "--------------------------------------------------" << std::endl;
 		return out;
 		} //end of operator<<
-	} // end of descr_qg struct
+
+
+		//! Import algorithm specifications from file .param
+		void import(ftool::md_param & fname)
+		{
+			FILE_ = fname;
+			MESH_FILEG  = FILE_.string_value("MESH_FILEG", "1D points file");
+			MESH_TYPEG  = FILE_.string_value("MESH_TYPEG", "1D mesh type");
+			FEM_TYPEG   = FILE_.string_value("FEM_TYPEG", "FEM type - solution");
+			FEM_TYPEG_DATA = FILE_.string_value("FEM_TYPEG_DATA", "FEM type - coefficients");
+			IM_TYPEG 	= FILE_.string_value("IM_TYPEG","Name of integration method");
+
+			// Set default values if empty strings are passed.
+			if(FEM_TYPEG_DATA == "") FEM_TYPEG_DATA = "FEM_PK(1,1)";
+
+			SOLVE_METHOD = FILE_.string_value("SOLVE_METHOD", "Name of solver");
+			if (SOLVE_METHOD != "SuperLU") { // iterative solver
+				MAX_ITER  = FILE_.int_value("MAX_ITER", "Max number of sub-iterations");
+			TOL = FILE_.real_value("TOL"); if (TOL == 0.) TOL = 2.0e-10;
+			}
+			OUTPUT = FILE_.string_value("OUTPUT","Output Directory");
+		} // end of import
+	}; // end of descr_qg struct
 
 } // end of namespace
 #endif
--- Prove/type_aliases.hpp
+++ Prove/type_aliases.hpp
@@ -9,13 +9,16 @@ namespace getfem {
 	using bgeot::scalar_type;
 	using bgeot::size_type;
 
-	typedef std::vector<size_type> vector_size_type
+	typedef std::vector<size_type> vector_size_type;
 	typedef std::vector<scalar_type> vector_type;
 
 	//gmm namespace
 	typedef gmm::rsvector<scalar_type> sparse_vector_type;
 	typedef gmm::row_matrix<sparse_vector_type> sparse_matrix_type;
+	typedef gmm::dense_matrix<scalar_type> dense_matrix_type;
 
 	//getfem namespace
 	//could be useful to avoid namespace getfem and putting namespace femg
 }
+
+#endif
