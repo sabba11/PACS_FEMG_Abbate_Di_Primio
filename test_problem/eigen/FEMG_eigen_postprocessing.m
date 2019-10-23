bd4f9c2b01fb434a7bb64c903e81055447905f6d
Andrea Di Primio <andrea.diprimio@mail.polimi.it>
2019-10-23 11:36:45 +0200

    Correcting last commit.

--- test_problem/eigen/FEMG_eigen_postprocessing.m
+++ test_problem/eigen/FEMG_eigen_postprocessing.m
@@ -29,6 +29,7 @@ eigvects = zeros(N, N); % matrix to store eigenvectors
 mf = gfMeshFem('load',[path_to_files, '/solution.mf']); % import mesh_fem object 
 mesh = gfMesh('load',[path_to_files, '/mesh.mh']); % import mesh object
 
+% importing data
 % eigenvalue file
 thisfile = eigen_files(1).name;
 U = load(thisfile);
@@ -43,30 +44,57 @@ for i = 2 : N+1
 end
 fclose('all');
 
-%% 3. Computing eigenvector oscillations
-
-compute_oscillations(eigvects, mf, mesh) ;
-
-%% 4. Eigenvalue plots
+%% 3. Eigenvalue plot
+% Plots eigenvalues in ascending order.
 
 figure()
 plot([1:N], eigvals, 'r-o', 'LineWidth', 0.1);
 title('Eigenvalues of the operator sorted by ascending order');
 xlabel('Eigenvalue index');
 ylabel('Eigenvalue');
+grid on
 
-%% 5. Eigenvector plots
+%% 4. Eigenvector plots
+% Plots the first n_plots eigenvectors.
 
-n_plots = 10;
+n_plots = 3;
 for i = 1 : n_plots
     figure()
     val = eigvals(i);
+    gf_plot(mf, 10*eigvects(:, i)', 'zplot', 'on', 'mesh', 'on', 'title', ['Eigenvector associated to \lambda = ', num2str(val)], 'disp_options', 'off');
     title(['Eigenvector associated to \lambda = ', num2str(val)]);
-    gf_plot(mf, eigvects(:, i)', 'zplot', 'on', 'mesh', 'on', 'title', ['Eigenvector associated to \lambda = ', num2str(val)]);
+    pbaspect([2 1 1]);
+    xlabel('x');
+    ylabel('y');
+    zlabel('Eigenvector');
+    grid on
+    colorbar
 end
 
+%% 5. Computing eigenvector oscillations
+% Computes the number of oscillations of all eigenvectors, plotting the 
+
+FEMG_compute_oscillations(eigvects, mf, mesh) ;
+
 %% 6. Saving eigenvalue data for further comparison
+% Saves eigenvalues to avoid variable overwriting upon postprocessing of
+% different numerical results.
 
 % FEMG_eigval_comparison();
 
-%% 7. 
+%% 7. Boundary conditions check
+% Checks boundary conditions for the i-th eigenvector. Includes a for cycle
+% if multiple eigenvectors are to be checked.
+
+idx = 2;
+FEMG_check_BC(eigvects(:,idx), mf, mesh);
+
+% for i = 1 : idx
+%   figure()
+%   
+% end
+
+%% 8. Other postprocessing
+% Space to define custom postprocessing routines.
+
+% ...
